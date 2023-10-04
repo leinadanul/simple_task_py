3957819ddb31bf318e331b6022f9672dcb7901da
Daniel Rmirez Luna <danielramire1996@gmail.com>
2023-10-04 13:01:54 -0500

    boostrap delete

--- TaskManager/TaskManager/settings.py
+++ TaskManager/TaskManager/settings.py
@@ -33,7 +33,6 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
 
     'tasks.apps.TasksConfig',
-    'bootstrap5',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
