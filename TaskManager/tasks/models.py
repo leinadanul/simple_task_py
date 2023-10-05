94eb34e9742708c83f51f0770a8aa1180df73dc2
Daniel Rmirez Luna <danielramire1996@gmail.com>
2023-10-05 17:41:48 -0500

    due_date_added

--- TaskManager/tasks/models.py
+++ TaskManager/tasks/models.py
@@ -19,6 +19,7 @@ class Task(models.Model):
     ]
     priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES, default='meduim')
     tags = models.ManyToManyField(Tag)
+    due_date = models.DateField(null= True, blank= True)
 
     def __str__(self):
         return self.title
