5d40bd0d9adb36ed620fc46e4adfe2351ad4ec31
Daniel Rmirez Luna <danielramire1996@gmail.com>
2023-10-05 17:12:33 -0500

    Priority is added and implemented tag for task added

--- TaskManager/tasks/models.py
+++ TaskManager/tasks/models.py
@@ -1,11 +1,27 @@
 from django.db import models
 
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
 class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    
+
+    PRIORITY_CHOICES = [
+        ('low', 'Low'),
+        ('medium', 'Medium'),
+        ('high', 'Hig'),
+    ]
+    priority = models.CharField(max_length=10, choices=PRIORITY_CHOICES, default='meduim')
+    tags = models.ManyToManyField(Tag)
 
     def __str__(self):
         return self.title
 
-# Create your models here.
+
+
